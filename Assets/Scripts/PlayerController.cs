e189909e0d1c3d81f39fa662834f95dea11fcfdf
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-12 04:55:36 -0500

    coin and other

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 6f;
     private Rigidbody rb;
+    private int score = 0;
+    private int health = 5;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     void Update()
     {
+        
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score += 1;
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Trap")
+        {
+            health -= 1;
+            Debug.Log("Health: " + health);
+            if (health == 0)
+            {
+                ReloadScene();
+            }
+        }
+    }
+
+    public void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 
     void FixedUpdate()
@@ -22,21 +52,25 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(transform.forward * speed);
+            
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(-transform.forward * speed);
+            
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(-transform.right * speed);
+            
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(transform.right * speed);
+            
         }
     }
 }
